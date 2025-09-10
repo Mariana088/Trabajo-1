fe7546b880ba7e939e9b4e8bf00b6ced20f3add9
Mariana088 <margarciahe@unal.edu.co>
2025-09-09 22:42:19 -0500

    Create ejercicio_3.py

---
+++ ejercicio_3.py
@@ -0,0 +1,13 @@
+#Datos dados
+Horas_trabajadas = 48
+pago_hora = 5000
+porcentaje_retencion = 0.125
+
+#Incognitas
+salariobruto = int(Horas_trabajadas*pago_hora)
+retencion = int(salariobruto*porcentaje_retencion)
+salarioneto = int(salariobruto-retencion)
+
+print(f" El salario bruto es: {salariobruto}")
+print(f" La retención de la fuente es: {retencion}")
+print(f" El salario neto es: {salarioneto}")
